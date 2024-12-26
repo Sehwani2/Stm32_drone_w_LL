bf6bb1caab4a68d4d703a4cefff8f9a71afb8d76
sehwan <hyn0216388@gmail.com>
2024-12-26 13:38:09 +0900

    BNO080 Calibration

--- Project_drone/Core/Src/main.c
+++ Project_drone/Core/Src/main.c
@@ -79,6 +79,7 @@ void SystemClock_Config(void);
 int Is_iBus_Throttle_Min(void);
 int Is_iBus_Received(void);
 void ESC_Calibration(void);
+void BNO080_Calibration(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -134,7 +135,6 @@ int main(void)
   /* USER CODE BEGIN 2 */
   LL_TIM_EnableCounter(TIM3);
 
-
   LL_USART_EnableIT_RXNE(USART6);
   LL_USART_EnableIT_RXNE(UART4);
   LL_USART_EnableIT_RXNE(UART5);
@@ -171,9 +171,27 @@ int main(void)
 	  TIM3->PSC = 1500;
 	  HAL_Delay(200);
 	  LL_TIM_CC_DisableChannel(TIM3, LL_TIM_CHANNEL_CH4);
-	  HAL_Delay(200);
 
 	  ESC_Calibration();
+	  while(iBus.SwC != 1000)
+	  {
+		  Is_iBus_Received();
+
+		  LL_TIM_CC_EnableChannel(TIM3,LL_TIM_CHANNEL_CH4);
+		  TIM3->PSC = 1500;
+		  HAL_Delay(200);
+		  LL_TIM_CC_DisableChannel(TIM3, LL_TIM_CHANNEL_CH4);
+		  HAL_Delay(200);
+	  }
+  }
+  else if(iBus.SwC == 1500)
+  {
+	  LL_TIM_CC_EnableChannel(TIM3,LL_TIM_CHANNEL_CH4);
+	  TIM3->PSC = 1500;
+	  HAL_Delay(200);
+	  LL_TIM_CC_DisableChannel(TIM3, LL_TIM_CHANNEL_CH4);
+
+	  BNO080_Calibration();
 	  while(iBus.SwC != 1000)
 	  {
 		  Is_iBus_Received();
@@ -194,17 +212,19 @@ int main(void)
 	  LL_TIM_CC_DisableChannel(TIM3, LL_TIM_CHANNEL_CH4);
 	  HAL_Delay(150);
   }
-
-  EP_PIDGain_Write(0, 1.1, 2.2, 3.3);
-  float p = 0.0, i = 0.0, d = 0.0;
-  EP_PIDGain_Read(0, &p, &i, &d);
-  printf("%f %f %f", p, i, d);
+// test EEPROM
+//  EP_PIDGain_Write(0, 1.1, 2.2, 3.3);
+//  float p = 0.0, i = 0.0, d = 0.0;
+//  EP_PIDGain_Read(0, &p, &i, &d);
+//  printf("%f %f %f", p, i, d);
 
   // buzzer on
   LL_TIM_CC_EnableChannel(TIM3,LL_TIM_CHANNEL_CH4);
   TIM3->PSC = 2000; // buzzer pwm
   //TIM3->CCR4 = TIM3->ARR/2; // PWM Width
   HAL_Delay(100);
+  TIM3->PSC = 1500; // buzzer pwm
+   HAL_Delay(100);
   TIM3->PSC = 1000; // buzzer pwm
   HAL_Delay(100);
   LL_TIM_CC_DisableChannel(TIM3, LL_TIM_CHANNEL_CH4);
@@ -216,11 +236,11 @@ int main(void)
   while (1)
   {
 
-    /* USER CODE END WHILE */
+	  /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+	  /* USER CODE BEGIN 3 */
 	  batVolt = adcVal * 0.003619f;
-	  printf("%d\t%.2f\n", adcVal, batVolt);
+	  //printf("%d\t%.2f\n", adcVal, batVolt);
 	  if(batVolt < 10.0f)
 	  {
 		  TIM3->PSC = 1000;
@@ -230,75 +250,75 @@ int main(void)
 	  {
 		  LL_TIM_CC_DisableChannel(TIM3, LL_TIM_CHANNEL_CH4);
 	  }
-	  HAL_Delay(100);
 
-//	 if(BNO080_dataAvailable() == 1)
-//	 {
-//		 q[0] = BNO080_getQuatI();
-//		 q[1] = BNO080_getQuatJ();
-//		 q[2] = BNO080_getQuatK();
-//		 q[3] = BNO080_getQuatReal();
-//		 quatRadianAccuracy = BNO080_getQuatRadianAccuracy();
-//
-//		 Quaternion_Update(&q[0]);
-//
-//		 printf("%f, %f, %f \n",BNO080_Roll*100,BNO080_Pitch*100,BNO080_Yaw*100);
-//	 }
-//	 if(ICM20602_DataReady() == 1)
-//	 {
-//		 ICM20602_Get3AxisGyroRawData(&ICM20602.gyro_x_raw);
-//
-//		 ICM20602.gyro_x = ICM20602.gyro_x_raw * 2000.f / 32768.f;
-//		 ICM20602.gyro_y = ICM20602.gyro_y_raw * 2000.f / 32768.f;
-//		 ICM20602.gyro_z = ICM20602.gyro_z_raw * 2000.f / 32768.f;
-//
-//		 printf("%f %f %f \n",ICM20602.gyro_x,ICM20602.gyro_y,ICM20602.gyro_z);
-//	 }
-//	  if(LPS22HH_DataReady() == 1)
-//	  {
-//		  LPS22HH_GetPressure(&LPS22HH.pressure_raw);
-//		  LPS22HH_GetTemperature(&LPS22HH.temperature_raw);
-//
-//		  LPS22HH.baroAlt = getAltitude2(LPS22HH.pressure_raw / 4096.f, LPS22HH.temperature_raw / 100.f);
-//
-//		  LPS22HH.baroAltFilt = LPS22HH.baroAltFilt * X  + LPS22HH.baroAlt * (1.0f - X );
-//
-//		  printf("%f ",LPS22HH.baroAlt * 100);
-//		  printf("%f\n",LPS22HH.baroAltFilt * 100);
-//	  }
-//	  if(m8n_rx_cplt_flag == 1)
-//	  {
-//		  m8n_rx_cplt_flag = 0;
-//
-//		  if( M8N_UBX_CHKSUM(&m8n_rx_buf[0],36) == 1)
-//		  {
-//			  LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_2);
-//			  M8N_UBX_POSLLH_Parsing(&m8n_rx_buf[0], &posllh);
-//
-//			  printf("LAT : %ld\t LON: %ld\t height : %ld",posllh.lat, posllh.lon, posllh.height);
-//		  }
-//	  }
+	  if(BNO080_dataAvailable() == 1)
+	  {
+		  q[0] = BNO080_getQuatI();
+		  q[1] = BNO080_getQuatJ();
+		  q[2] = BNO080_getQuatK();
+		  q[3] = BNO080_getQuatReal();
+		  quatRadianAccuracy = BNO080_getQuatRadianAccuracy();
+
+		  Quaternion_Update(&q[0]);
+
+		 printf("%.2f\t%.2f\n", BNO080_Roll, BNO080_Pitch);
+		 printf("%.2f\n", BNO080_Yaw);
+	  }
+	  //	 if(ICM20602_DataReady() == 1)
+	  //	 {
+	  //		 ICM20602_Get3AxisGyroRawData(&ICM20602.gyro_x_raw);
+	  //
+	  //		 ICM20602.gyro_x = ICM20602.gyro_x_raw * 2000.f / 32768.f;
+	  //		 ICM20602.gyro_y = ICM20602.gyro_y_raw * 2000.f / 32768.f;
+	  //		 ICM20602.gyro_z = ICM20602.gyro_z_raw * 2000.f / 32768.f;
+	  //
+	  //		 printf("%f %f %f \n",ICM20602.gyro_x,ICM20602.gyro_y,ICM20602.gyro_z);
+	  //	 }
+	  //	  if(LPS22HH_DataReady() == 1)
+	  //	  {
+	  //		  LPS22HH_GetPressure(&LPS22HH.pressure_raw);
+	  //		  LPS22HH_GetTemperature(&LPS22HH.temperature_raw);
+	  //
+	  //		  LPS22HH.baroAlt = getAltitude2(LPS22HH.pressure_raw / 4096.f, LPS22HH.temperature_raw / 100.f);
+	  //
+	  //		  LPS22HH.baroAltFilt = LPS22HH.baroAltFilt * X  + LPS22HH.baroAlt * (1.0f - X );
+	  //
+	  //		  printf("%f ",LPS22HH.baroAlt * 100);
+	  //		  printf("%f\n",LPS22HH.baroAltFilt * 100);
+	  //	  }
+	  //	  if(m8n_rx_cplt_flag == 1)
+	  //	  {
+	  //		  m8n_rx_cplt_flag = 0;
+	  //
+	  //		  if( M8N_UBX_CHKSUM(&m8n_rx_buf[0],36) == 1)
+	  //		  {
+	  //			  LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_2);
+	  //			  M8N_UBX_POSLLH_Parsing(&m8n_rx_buf[0], &posllh);
+	  //
+	  //			  printf("LAT : %ld\t LON: %ld\t height : %ld",posllh.lat, posllh.lon, posllh.height);
+	  //		  }
+	  //	  }
 
 	  if(ibus_rx_cplt_flag == 1)
 	  {
 		  ibus_rx_cplt_flag = 0;
 		  if(iBus_Check_CHKSUM(&ibus_rx_buf[0],32) == 1)
 		  {
-			LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_2);
-			iBus_Parsing(&ibus_rx_buf[0], &iBus);
+			  LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_2);
+			  iBus_Parsing(&ibus_rx_buf[0], &iBus);
 
-			if(iBus_isActiveFailsafe(&iBus) == 1)
-			{
-				LL_TIM_CC_EnableChannel(TIM3,LL_TIM_CHANNEL_CH4);
-			}
-			else
-			{
-				LL_TIM_CC_DisableChannel(TIM3, LL_TIM_CHANNEL_CH4);
-			}
-//			 printf("%d\t%d\t%d\t%d\t%d\t%d\n",
-//					iBus.RH, iBus.RV, iBus.LV, iBus.LH, iBus.SwA, iBus.SwC);
-//
-//			 HAL_Delay(100);
+			  if(iBus_isActiveFailsafe(&iBus) == 1)
+			  {
+				  LL_TIM_CC_EnableChannel(TIM3,LL_TIM_CHANNEL_CH4);
+			  }
+			  else
+			  {
+				  LL_TIM_CC_DisableChannel(TIM3, LL_TIM_CHANNEL_CH4);
+			  }
+			  //			 printf("%d\t%d\t%d\t%d\t%d\t%d\n",
+			  //					iBus.RH, iBus.RV, iBus.LV, iBus.LH, iBus.SwA, iBus.SwC);
+			  //
+			  //			 HAL_Delay(100);
 		  }
 	  }
 
@@ -397,6 +417,127 @@ void ESC_Calibration(void)
 	  HAL_Delay(8000);
 }
 
+void BNO080_Calibration(void)
+{
+	//Resets BNO080 to disable All output
+	BNO080_Initialization();
+
+	//BNO080/BNO085 Configuration
+	//Enable dynamic calibration for accelerometer, gyroscope, and magnetometer
+	//Enable Game Rotation Vector output
+	//Enable Magnetic Field output
+	BNO080_calibrateAll(); //Turn on cal for Accel, Gyro, and Mag
+	BNO080_enableGameRotationVector(20000); //Send data update every 20ms (50Hz)
+	BNO080_enableMagnetometer(20000); //Send data update every 20ms (50Hz)
+
+	//Once magnetic field is 2 or 3, run the Save DCD Now command
+  	printf("Calibrating BNO080. Pull up FS-i6 SWC to end calibration and save to flash\n");
+  	printf("Output in form x, y, z, in uTesla\n\n");
+
+	//while loop for calibration procedure
+	//Iterates until iBus.SwC is mid point (1500)
+	//Calibration procedure should be done while this loop is in iteration.
+	while(iBus.SwC == 1500)
+	{
+		if(BNO080_dataAvailable() == 1)
+		{
+			//Observing the status bit of the magnetic field output
+			float x = BNO080_getMagX();
+			float y = BNO080_getMagY();
+			float z = BNO080_getMagZ();
+			unsigned char accuracy = BNO080_getMagAccuracy();
+
+			float quatI = BNO080_getQuatI();
+			float quatJ = BNO080_getQuatJ();
+			float quatK = BNO080_getQuatK();
+			float quatReal = BNO080_getQuatReal();
+			unsigned char sensorAccuracy = BNO080_getQuatAccuracy();
+
+			printf("%f,%f,%f,", x, y, z);
+			if (accuracy == 0) printf("Unreliable\t");
+			else if (accuracy == 1) printf("Low\t");
+			else if (accuracy == 2) printf("Medium\t");
+			else if (accuracy == 3) printf("High\t");
+
+			printf("\t%f,%f,%f,%f,", quatI, quatI, quatI, quatReal);
+			if (sensorAccuracy == 0) printf("Unreliable\n");
+			else if (sensorAccuracy == 1) printf("Low\n");
+			else if (sensorAccuracy == 2) printf("Medium\n");
+			else if (sensorAccuracy == 3) printf("High\n");
+
+			//Turn the LED and buzzer on when both accuracy and sensorAccuracy is high
+			if(accuracy == 3 && sensorAccuracy == 3)
+			{
+				LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2);
+				TIM3->PSC = 65000; //Very low frequency
+				LL_TIM_CC_EnableChannel(TIM3, LL_TIM_CHANNEL_CH4);
+			}
+			else
+			{
+				LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2);
+				LL_TIM_CC_DisableChannel(TIM3, LL_TIM_CHANNEL_CH4);
+			}
+		}
+
+		Is_iBus_Received(); //Refreshes iBus Data for iBus.SwC
+		HAL_Delay(100);
+	}
+
+	//Ends the loop when iBus.SwC is not mid point
+	//Turn the LED and buzzer off
+	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2);
+	LL_TIM_CC_DisableChannel(TIM3, LL_TIM_CHANNEL_CH4);
+
+	//Saves the current dynamic calibration data (DCD) to memory
+	//Sends command to get the latest calibration status
+	BNO080_saveCalibration();
+	BNO080_requestCalibrationStatus();
+
+	//Wait for calibration response, timeout if no response
+	int counter = 100;
+	while(1)
+	{
+		if(--counter == 0) break;
+		if(BNO080_dataAvailable())
+		{
+			//The IMU can report many different things. We must wait
+			//for the ME Calibration Response Status byte to go to zero
+			if(BNO080_calibrationComplete() == 1)
+			{
+				printf("\nCalibration data successfully stored\n");
+				LL_TIM_CC_EnableChannel(TIM3, LL_TIM_CHANNEL_CH4);
+				TIM3->PSC = 2000;
+				HAL_Delay(300);
+				TIM3->PSC = 1500;
+				HAL_Delay(300);
+				LL_TIM_CC_DisableChannel(TIM3, LL_TIM_CHANNEL_CH4);
+				HAL_Delay(1000);
+				break;
+			}
+		}
+		HAL_Delay(10);
+	}
+	if(counter == 0)
+	{
+		printf("\nCalibration data failed to store. Please try again.\n");
+		LL_TIM_CC_EnableChannel(TIM3, LL_TIM_CHANNEL_CH4);
+		TIM3->PSC = 1500;
+		HAL_Delay(300);
+		TIM3->PSC = 2000;
+		HAL_Delay(300);
+		LL_TIM_CC_DisableChannel(TIM3, LL_TIM_CHANNEL_CH4);
+		HAL_Delay(1000);
+	}
+
+	//BNO080_endCalibration(); //Turns off all calibration
+	//In general, calibration should be left on at all times. The BNO080
+	//auto-calibrates and auto-records cal data roughly every 5 minutes
+
+	//Resets BNO080 to disable Game Rotation Vector and Magnetometer
+	//Enables Rotation Vector
+	BNO080_Initialization();
+	BNO080_enableRotationVector(2500); //Send data update every 2.5ms (400Hz)
+}
 
 /* USER CODE END 4 */
 
